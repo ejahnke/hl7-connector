f22db02317d37519dcf57a5d05538a64335cfb7c
EC2 Default User <ec2-user@ip-172-31-14-137.ca-central-1.compute.internal>
2022-12-20 03:24:50 +0000

    Initial commit

---
+++ test/hl7-connector.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Hl7Connector from '../lib/hl7-connector-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hl7-connector-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Hl7Connector.Hl7ConnectorStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
